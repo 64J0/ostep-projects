ff69b1641fe9b4941c5ba4d050f3ff87d75f2e20
64J0 <vinigaio97@gmail.com>
2024-09-24 10:11:43 -0300

    improve wcat.c

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -11,8 +11,9 @@
 #define SUCCESS_EXIT 0
 #define ERROR_EXIT 1
 
-/* We assume that the file this program is reading is a text file and that lines in this text file are no longer than 1024 bytes long. */
-#define LINE_SIZE 1024
+/* We assume that the file this program is reading is a text file and that lines
+ * in this text file are no longer than 1024 bytes long. */
+#define LINE_SIZE 256
 
 int main(int argc, char **argv) {
   if (argc < 2) {
@@ -20,20 +21,21 @@ int main(int argc, char **argv) {
     return SUCCESS_EXIT;
   }
 
+  char *buffer;
+  buffer = malloc(sizeof(char) * (LINE_SIZE + 1));
   for (int i = 1; i < argc; ++i) {
-    char* pathname = argv[i];
-    
-    FILE* f = fopen(pathname, "r");
+    char *pathname = argv[i];
+
+    FILE *f = fopen(pathname, "r");
     if (f == NULL) {
       printf("wcat: cannot open file\n");
       return ERROR_EXIT;
     }
 
-    char *buffer;
-    buffer = malloc(LINE_SIZE + 1);
-    while(fgets(buffer, LINE_SIZE + 1, f) != NULL) {
-      if (buffer != NULL)
+    while (fgets(buffer, LINE_SIZE + 1, f) != NULL) {
+      if (buffer != NULL) {
         printf("%s", buffer);
+      }
     }
 
     int c = fclose(f);
@@ -41,6 +43,8 @@ int main(int argc, char **argv) {
       printf("wcat: cannot open file\n");
       return ERROR_EXIT;
     }
+  }
+  if (buffer != NULL) {
     free(buffer);
   }
 
